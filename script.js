a69dfc29bbf59c5d2c7b85b3efb8ebf9d2c5ca3e
Nxyz-natan <natanyemane75@icloud.com>
2026-06-14 01:44:38 -0600

    made it actually work now i can place circle or crosses

--- script.js
+++ script.js
@@ -17,8 +17,22 @@ cellElements.forEach((cell, index) => {
 function placeMarker(index) {
 let col = index % 3 
 let row = (index - col) / 3
-
+if(boardData [row][col] == 0){
 boardData [row][col] = Player;
 Player *= -1;
+drawMarkers();
+}
+}
 
+
+function drawMarkers() {
+    for(let row = 0; row < 3; row++) {
+        for(let col = 0; col < 3; col++) {
+            if(boardData[row][col] == 1) {
+                cellElements[(row * 3) + col].classList.add("cross");
+            } else if(boardData[row][col] == -1) {
+                cellElements[(row * 3) + col].classList.add("circle");
+            }
+        }
+    }
 }
